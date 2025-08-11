839d4ab16cdd8b648ab3ef134e05b64422dd46dc
S Dermoumi <hi@sdrm.me>
2025-08-11 22:15:26 +0000

    Refactor db::utils

--- packages/backend/src/database/mod.rs
+++ packages/backend/src/database/mod.rs
@@ -8,5 +8,5 @@ pub use migrate::Migrate;
 pub use migrate::Migration;
 pub use setup::Pool;
 pub use setup::init;
-pub use utils::TimestampWithTimeZone;
+pub use utils::TzTimestamp;
 pub use utils::with_transaction;
---
+++ packages/backend/src/database/utils/mod.rs
@@ -0,0 +1,5 @@
+mod tztimestamp;
+mod with_transaction;
+
+pub use tztimestamp::TzTimestamp;
+pub use with_transaction::with_transaction;
