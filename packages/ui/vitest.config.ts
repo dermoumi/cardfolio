3906a6c4e2894426ddd3576b32a57989b80ee9a2
S Dermoumi <hi@sdrm.me>
2025-09-12 18:39:31 +0000

    Update test config

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -1,23 +1,9 @@
-import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000/",
-        changeOrigin: true,
-      },
-    },
   },
   css: {
     modules: {
