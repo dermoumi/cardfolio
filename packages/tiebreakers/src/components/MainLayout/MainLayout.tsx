53c5f9d6d97e4cd89c9f4cfe1e2d91c106ed4e96
S Dermoumi <hi@sdrm.me>
2025-10-01 20:09:37 +0000

    Remove tanstack devtools

--- packages/tiebreakers/src/components/MainLayout/MainLayout.tsx
+++ packages/tiebreakers/src/components/MainLayout/MainLayout.tsx
@@ -1,17 +1,11 @@
 import type { FC, PropsWithChildren } from "react";
 
 import { Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export type MainLayoutProps = PropsWithChildren;
 
 const MainLayout: FC<MainLayoutProps> = () => {
-  return (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools position="top-right" />
-    </>
-  );
+  return <Outlet />;
 };
 
 export default MainLayout;
