2e1025ca6fccd05a1ecce983c08a765e8f22f403
S Dermoumi <hi@sdrm.me>
2025-12-11 04:40:18 +0000

    Setup storybook docs

--- packages/ui/src/components/BaseButton/BaseButton.stories.tsx
+++ packages/ui/src/components/BaseButton/BaseButton.stories.tsx
@@ -1,9 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
+import { fn } from "storybook/test";
+
 import BaseButton from ".";
 
 const meta = {
+  title: "Atoms/BaseButton",
   component: BaseButton,
+  parameters: { layout: "centered" },
+  args: {
+    onClick: fn(),
+  },
+  argTypes: {
+    disabled: { control: "boolean" },
+    children: { description: "Content to be displayed inside the button." },
+  },
 } satisfies Meta<typeof BaseButton>;
 export default meta;
 
--- packages/ui/src/components/BaseButton/BaseButton.tsx
+++ packages/ui/src/components/BaseButton/BaseButton.tsx
@@ -3,7 +3,13 @@ import type { FC, PropsWithChildren } from "react";
 import styles from "./BaseButton.module.css";
 
 export type BaseButtonProps = PropsWithChildren<{
+  /**
+   * Click handler for the button.
+   */
   onClick?: () => void;
+  /**
+   * Disables the button if true.
+   */
   disabled?: boolean;
 }>;
 
