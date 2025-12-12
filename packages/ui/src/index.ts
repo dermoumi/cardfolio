0a02de2a5a7d96c12bf7aa2bb4ce831440325a0c
S Dermoumi <hi@sdrm.me>
2025-12-12 08:19:46 +0000

    Add ColorSchemeProvider

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,3 +3,6 @@ export * from "./components/Button";
 
 export { default as TextInput } from "./components/TextInput";
 export * from "./components/TextInput";
+
+export { default as ColorSchemeProvider } from "./providers/ColorSchemeProvider";
+export * from "./providers/ColorSchemeProvider";
